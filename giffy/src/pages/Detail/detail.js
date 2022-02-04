1075684fe08905ef56e139831ba993cb98e20a17
VECTORITCGROUP\dgomez <dgomez@vectoritcgroup.com>
2022-02-04 13:24:35 +0100

    nuevo customHook para leer el context.

--- giffy/src/pages/Detail/detail.js
+++ giffy/src/pages/Detail/detail.js
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
 import Gif from "../../components/Gif/Gif";
-import GifsContext from "../../context/GifsContext";
+import useGlobalGifs from "../../hooks/useGlobalsGifs";
+// import GifsContext from "../../context/GifsContext";
 
 export default function Detail ({ params }) {
-    const {gifs} = useContext(GifsContext)
+    const gifs = useGlobalGifs()
     console.log({gifs});
 
     const gif = gifs.find(singleGif => singleGif.id === params.id)
